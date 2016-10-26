eb244f6bccc912585fcc12c70a63f26305897111
Kourion <patrick.ludwig@stud-mail.uni-wuerzburg.de>
2016-10-26 22:27:33 +0200

    Added class and main menthod frame

--- BDYgui.java
+++ BDYgui.java
@@ -1,1 +1,10 @@
 #GUI file
+    #testing git
+
+class BDYgui extends Parent implements Interface {
+
+    public static void main(String[] args) {
+        #This is on another Branch yay
+    }
+
+}
