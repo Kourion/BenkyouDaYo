cec1e0de8fb8b8dd4bf4764961da26f5ebd5c2b0
Kourion <Kourion@users.noreply.github.com>
2016-12-11 18:01:57 +0100

    Create JapSem3tempChap9.cpp

---
+++ JapSem3tempChap9.cpp
@@ -0,0 +1,32 @@
+\section{Expressions of Experience}
+\begin{itemize}
+    \item N1 は...V-た  こと が あります. = N1 has the experience of V-ing.
+    
+    \item N1 は...V-dic こと が あります. = It sometimes happens that N1 does V
+                                         = There are cases in which N1 does V
+\end{itemize}
+\subsection{Counting Experience}
+\begin{itemize}
+\item Asking how often did one epperience something: \\
+    \begin{tabular}{ l l l l l l l l }
+    あなたは&なんかい&それを&した&ことがありますか。\\
+            &なんど  &      &    &                  \\
+    \end{tabular}
+\item Modifying counting words: \\    
+    \begin{tabular}{ l l l l l l l }
+    how often   & once      & twice     & thrice    & several times & many times \\
+    なんかい    & いっかい  & にかい    & さんかい  & なんかいか    & なんかいも \\
+    なんど      & いちど    & にど      & さんど    & なんどか      & なんども 
+    \end{tabular}
+\item Further question words which can be modified:
+\begin{itemize}
+\item \ruby{何時}{いつ} = when, \tabni --> \tabni \ruby{何時}{いつ}か = someday, \tabni --> \ruby{何時}{いつ}も = always/usually/every time |never (with neg. verb)
+
+\item \ruby{何処}{どこ} = where, \tabni --> \tabni \ruby{何処}{どこ}か = somewhere, \tabni --> \tabni \ruby{何処}{どこ}も = everywhere/wherever |nowhere (with neg. verb)
+
+\item \ruby{何}{なに} = what, \tabni --> \tabni \ruby{何}{なに}か = something, \tabni --> \tabni \ruby{何}{なに}も = all |nothing (with neg. verb)
+
+\item \ruby{誰}{だれ} = who, \tabni --> \tabni \ruby{誰}{だれ}か = someone, \tabni --> \tabni  \ruby{誰}{だれ}も = everyone /anyone |no-one (with neg. verb)
+\end{itemize}
+も can here also always be replaced with にも for further modification of the question words. \\
+\end{itemize}
